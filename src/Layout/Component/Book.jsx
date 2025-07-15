e8d1fa3f3cd38fbb345406ac1fe0bddf52cef000
sahincoder <sahincoderbd@gmail.com>
2025-07-15 07:21:34 +0600

    fake data added and hero section added

--- src/Layout/Component/Book.jsx
+++ src/Layout/Component/Book.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 
 const Book = ({book}) => {
+    const {
+      name, image, author, totalPages,rating,category,tags,publisher,yearOfPublishing  
+    } = book;
     return (
         <div className=' rounded-lg shadow-lg p-4 hover:shadow-xl transition-shadow duration-300 flex flex-col flex-nowrap gap-4' >
+            <img src={image} alt="" />
             <h2>{book.name}</h2>
         </div>
     );
